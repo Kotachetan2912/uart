c452fdbce74ac19378f0d8684b4701da24dc8523
Kotachetan2912 <chetankota26@gmail.com>
2026-03-30 00:29:12 +0530

    reciever design source

---
+++ UART.srcs/sources_1/new/reciver.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.03.2026 00:28:02
+// Design Name: 
+// Module Name: reciver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reciver(
+
+    );
+endmodule
